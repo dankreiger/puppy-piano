8a485f7b3edb811ba9d88ca1d1098f93776c0262
Dan Kreiger <dan@dankreiger.com>
2018-07-10 03:22:29 +0200

    add puppy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { PianoComponent } from './piano/piano.component';
 import { PianoPipe } from './piano/piano.pipe';
+import { PuppyComponent } from './puppy/puppy.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     PianoComponent,
-    PianoPipe
+    PianoPipe,
+    PuppyComponent
   ],
   imports: [
     BrowserModule,
