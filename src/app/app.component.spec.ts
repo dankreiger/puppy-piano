ffca2d462475902bb5afb32eeb8636172fb4b9c0
Daniel Kreiger <d.kreiger@compuccino.com>
2018-07-10 10:12:08 +0200

    add travis

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,20 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PianoComponent } from './piano/piano.component';
+import { PuppyComponent } from './puppy/puppy.component';
+import { PianoPipe } from './piano/piano.pipe';
+
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
+        PianoComponent,
+        PianoPipe,
+        PuppyComponent
       ],
     }).compileComponents();
   }));
